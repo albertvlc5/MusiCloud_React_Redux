4726152a96e87619ff91141d39de255a4e9ef8a7
Albert Pla <alplsan@epsa.upv.es>
2019-10-27 10:55:47 +0100

    implementacion sin buscador de canciones

---
+++ src/components/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import './App.css';
+import Grid from './Grid'
+import MusicBar from './MusicBar'
+
+import { connect } from "react-redux"
+import {fetchTracks} from "../actions/actions_tracks"
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.props.fetchTracks("Eminem");
+  }
+  
+  
+  render() {
+    return (
+      
+      <div>
+        <div>
+            &#127926; <input type="search" name="busqueda" placeholder="Introduzca el artista.."/>
+            <button type="submit">Buscar</button>
+        </div>
+        
+        <div className="loader" style={{ display:(this.props.tracksObj.fetching)?'block': 'none' }}>
+          <img alt="" src="/img/loading.gif"></img>
+        </div>
+
+        <div style={{display:(this.props.tracksObj.fetched)?'block': 'none'}}>
+          {
+            this.props.tracksObj.tracks.map((track, key) => {
+              return <Grid track={track} key={key} />;
+            })
+          }
+          <MusicBar/>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+
+function mapDispatchToProps(dispatch) {
+  return({
+    fetchTracks: (id) => {dispatch(fetchTracks(id))}
+  })
+}
+
+
+function mapStateToProps(store){
+  return {
+    tracksObj : store.tracksReducer
+  }
+}
+
+export default connect( mapStateToProps, mapDispatchToProps  )(App);
